b8e3f9a4a0c4cc87d495d934a7721a807a89c321
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-07-01 21:13:59 +0000

    Whitelist columns in CirclesContact for ActiveAdmin

--- app/admin/circles_contacts.rb
+++ app/admin/circles_contacts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CirclesContact do
 
+ permit_params :circle_id, :contact_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
