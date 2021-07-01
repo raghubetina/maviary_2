22f61ec107ffe6633d385a3afb11dd7245dc081d
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-07-01 21:15:49 +0000

    Add doorkeeper associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   # Direct associations
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
 
   has_many   :messages,
              :dependent => :destroy
