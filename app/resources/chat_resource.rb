ecfe4b24b597b4041244ff336783e6a4be978e46
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-07-01 21:15:25 +0000

    Generate graphiti resources for chats

---
+++ app/resources/chat_resource.rb
@@ -0,0 +1,19 @@
+class ChatResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :creator_id, :integer
+  attribute :picture, :string
+  attribute :topic, :string
+  attribute :link, :string
+  attribute :cover, :string
+  attribute :event, :boolean
+  attribute :starts_at, :datetime
+  attribute :ends_at, :datetime
+
+  # Direct associations
+
+  # Indirect associations
+
+end
