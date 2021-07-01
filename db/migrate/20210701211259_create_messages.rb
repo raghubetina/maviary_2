b2ce7216ab71ede308935b4eab95b3d2b3acaf56
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-07-01 21:13:00 +0000

    Generate messages resource with ideas:scaffold

---
+++ db/migrate/20210701211259_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :messages do |t|
+      t.text :body
+      t.string :ancestry
+      t.integer :chat_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
