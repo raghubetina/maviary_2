7d8107e40149dd17ab4c6c0eeb4ea1ae5a009cab
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-07-01 21:15:02 +0000

    Only circle user can modify circle

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,4 +1,6 @@
 class CirclesController < ApplicationController
+  before_action :current_user_must_be_circle_user, only: [:edit, :update, :destroy] 
+
   before_action :set_circle, only: [:show, :edit, :update, :destroy]
 
   # GET /circles
@@ -58,6 +60,14 @@ class CirclesController < ApplicationController
 
 
   private
+
+  def current_user_must_be_circle_user
+    set_circle
+    unless current_user == @circle.user
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_circle
       @circle = Circle.find(params[:id])
