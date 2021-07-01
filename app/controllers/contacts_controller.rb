aaa169386e686263a89daf3bd2ff1aa2d69559da
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-07-01 21:15:02 +0000

    Display only signed in users contacts on index

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
 
   # GET /contacts
   def index
-    @contacts = Contact.all
+    @contacts = current_user.contacts_as_adder.page(params[:page]).per(10)
   end
 
   # GET /contacts/1
