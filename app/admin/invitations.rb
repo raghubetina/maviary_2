ccd505ef25f78fcd74fdeb4580e1dd0623da7225
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-07-01 21:13:53 +0000

    Whitelist columns in Invitation for ActiveAdmin

--- app/admin/invitations.rb
+++ app/admin/invitations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Invitation do
 
+ permit_params :chat_id, :user_id, :token, :clicked, :rsvp_status
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
